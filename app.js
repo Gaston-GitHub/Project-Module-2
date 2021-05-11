aa7fff5d5b6715eebc78c950c92b0e6b9a2f5de8
“Genaro <ramoon1996@hotmail.com>
2021-05-11 19:49:12 +0200

    modificate appjs and seed archive created

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.use(
 );
 
 app.use('/', indexRouter);
-app.use('/users', authRouter);
+app.use('/', authRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

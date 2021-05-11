f218785df82d4777d8f3fcc09e7e6456f92a4dd9
Gaston <gaston@github.com>
2021-05-11 20:55:18 +0200

    profile route completed

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const bcryptjs = require('bcryptjs');
 
-const User = require('...model/user');
+const User = require("../models/user");
+
+const checkIfUserIsLoggedIn = require('../middlewares/auth')
+
 const router = express.Router();
+
+
 
 
 
 
 router.get('/signup', (req, res, next) => {
     res.render('auth/signup');
+});
+
+router.get('/login', (req, res, next) => {
+    res.render('auth/login')
+});
+
+router.get('/profile', checkIfUserIsLoggedIn, (req, res, next) => {
+    console.log('user', req.session.currentUser);
+    res.render('auth/profile', {user: req.session.currentUser})
 })
 
 router.post('/signup', (req, res, next) => {
@@ -27,6 +41,29 @@ router.post('/signup', (req, res, next) => {
             res.redirect('/')
         })
         .catch(error => next(error))
+})
+
+router.post('/login', (req, res, next) => {
+    const { email, password} = req.body;
+
+    User.findOne({email})
+    .then(dbUser => {
+        if(!dbUser) {
+            return res.render('auth/login', {error: 'user not found'});
+        }
+        const {_id, email: eMail, hashedPassword} = dbUser;
+        if(bcryptjs.compareSync(password, hashedPassword)) {
+            req.session.currentUser = {
+                _id, 
+                email: eMail,
+            };
+            return res.redirect('/profile');
+        }
+        return res.render('auth/login', {error: 'password incorrect'});
+    })
+    .catch(error => {
+        next(error);
+    })
 })
 
 

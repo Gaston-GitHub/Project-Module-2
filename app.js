bbe2c019aefd0e783178342a2471c42755602dbb
“Genaro <ramoon1996@hotmail.com>
2021-05-11 19:02:24 +0200

    app.js configurated

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')
 
-var app = express();
+const indexRouter = require('./routes/index');
+const authRouter = require('./routes/users');
+
+mongoose
+  .connect('mongodb://localhost:27017/digital-commerces', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to DB 🚀');
+  })
+  .catch(error => {
+    console.log('error ', error);
+  });
+
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +33,24 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(
+  session({
+    store: MongoStore.create({
+      mongoUrl: 'mongodb://localhost:27017/digital-commerces',
+      // mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60,
+    }),
+    secret: 'Digitalcommerces', // should be inside .env
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000,
+    },
+  }),
+);
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', authRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -40,6 +70,3 @@ app.use(function(err, req, res, next) {
 
 module.exports = app;
 
-
-test git flow modificado //editado
-test git flow 2 Genaro
